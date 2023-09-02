6779de72a2b7fcdf9c608cb0b838109fca4964d8
Bruno <brunoobispo@duck.com>
2023-09-02 14:38:22 -0300

    Copilot

--- init.lua
+++ init.lua
@@ -7,4 +7,5 @@
 -- })
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.g.copilot_assume_mapped = true
